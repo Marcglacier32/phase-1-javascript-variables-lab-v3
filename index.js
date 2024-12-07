d49847e2530484a141d7cafdc4655dca93be33cf
Mark Thiongo <mark.thiongo@student.moringaschool.com>
2024-12-07 09:57:49 +0300

    Define variables to pass tests

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+// Code your solution in this file!
+
+// 1. Define companyName as a constant
+const companyName = "Scuber";
+
+// 2. Define mostProfitableNeighborhood using let
+let mostProfitableNeighborhood = "Chelsea";
+
+// 3. Define companyCeo using let
+let companyCeo = "Susan Smith";
